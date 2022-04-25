8b751ca7f68eb87c3d8726d357f5fc0b4e818d45
xtotdam <xtotdam@yandex.ru>
2022-04-25 16:03:11 +0300

    rewrite with gmail as main channel and matrix almost removed

--- config.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Config():
-    TS = 'ts'
-
-    credentials = 'credentials-test.json'
-
--- ts-finished.py
+++ ts-finished.py
@@ -2,31 +2,29 @@
 
 import os, sys
 import subprocess as sp
-from datetime import timedelta
-from pathlib import Path
+from datetime import timedelta, datetime
+from pprint import pformat
+import logging
+import json
+import argparse
 
-from config import Config as C
+import smtplib
+from email.message import EmailMessage
 
-sys.stderr = open('err.log', 'w')
-sys.stdout = open('out.log', 'w')
 
-jobid, error, outfile, command = sys.argv[1:]
-
-# queue length
-jobs_queued = sp.check_output([C.TS, '-l']).decode('utf-8').count('queued')
-
-# job info
-ji = sp.check_output([C.TS, '-i', jobid]).decode('utf-8').split('\n')
-ji = list(filter(bool, ji))
-
-cwd = ji[1]
-
-# human readable time
-time = ji[-1].split(': ')[-1][:-1]
-ji[-1] = ji[-1] + '  ({})'.format(str(timedelta(seconds=float(time))))
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(funcName)s -- %(message)s',
+    filename='tsfinished.log',
+    level=logging.INFO
+)
 
 
 def transform_range_or_pass(s):
+    '''
+    transforms expanded ranges of numbers back into compressed representation
+    e.g. 1 2 3 4 5 -> {1..5}
+    on fail returns argument
+    '''
     only_digits = s.replace(' ', '').isdecimal()
     two_or_more = len(s.split()) >= 2
     if not (only_digits and two_or_more):
@@ -48,36 +46,97 @@ def transform_range_or_pass(s):
             return s
 
 
-cparts = command.split(':::')
-for i in range(len(cparts)):
-    cparts[i] = transform_range_or_pass(cparts[i].strip())
-command = ' ::: '.join(cparts)
+def parse_input(argv):
+    jobid, error, outfile, command = argv
 
-ji = '\n'.join(ji[3:])
+    # queue length
+    jobs_queued = sp.check_output([C['TS'], '-l']).decode('utf-8').count('queued')
 
-output = '''\
--------------------------------
-[TS] finished job {jobid}
+    # job info
+    ji = sp.check_output([C['TS'], '-i', jobid]).decode('utf-8').split('\n')
+    ji = list(filter(bool, ji))
 
-Exit status: {error}
-{ji}
-{jobs_queued} job{s} left
+    ### JI:
+    # 0 'Environment:'
+    # 1 TS_ENV
+    # 2 command         argv[3]
+    # 3 slots
+    # 4 enqueue time
+    # 5 start time
+    # 6 running time
 
-CWD: {cwd}
-{command}
--------------------------------
-'''.format(jobid=jobid, error=error, ji=ji.strip(),
-    jobs_queued=jobs_queued, cwd=cwd, command=command,
-    s='s' if jobs_queued != 1 else '')
+    if ';' in ji[1]:
+        hostname, cwd = ji[1].split(';')
+    else:
+        cwd = ji[1]
+        hostname = ''
 
-print(output)
+    # human readable time
+    time = ji[-1].split(': ')[-1][:-1]
+    ji[-1] = ji[-1] + '  ({})'.format(str(timedelta(seconds=float(time))))
+
+    # compress ranges
+    cparts = command.split(':::')
+    for i in range(len(cparts)):
+        cparts[i] = transform_range_or_pass(cparts[i].strip())
+    command = ' ::: '.join(cparts)
+
+    ji = '\n'.join(ji[3:])
+
+    error_message = ''
+    if error != '0':
+        error_message = 'Error occured!'
+
+    output = '''\
+    Exit status: {error} {error_message}
+    {ji}
+    {jobs_queued} job{s} left
+
+    Hostname: {hostname}
+    CWD: {cwd}
+    {command}
+    '''.format(jobid=jobid, error=error, ji=ji.strip(), error_message=error_message,
+        jobs_queued=jobs_queued, cwd=cwd, command=command, hostname=hostname,
+        s='s' if jobs_queued != 1 else '')
+
+    subject = '[TS] finished job {jobid} - {jobs_queued} left - {hostname}'.format(
+        jobid=jobid, jobs_queued=jobs_queued, hostname=hostname)
+
+    return subject, output
+
+
+def send_gmail(creds, subject, message):
+    msg = EmailMessage()
+
+    msg['Subject'] = subject
+    msg['From'] = creds['username']
+    msg['To'] = creds['recipients']
+    msg.set_content(message + '\n\n---\nSent by robot;)')
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(creds['username'], creds['password'])
+    server.send_message(msg)
+    server.quit()
 
 
 
-p = sp.Popen([
-    'python', 'matrix-nio-send/matrix-nio-send.py',
-    '-t', 'matrix-nio-send' + os.sep + C.credentials,
-    ],
-    stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.STDOUT)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Task spooler notifoer')
+    parser.add_argument('-n', '--dry-run', action='store_true', help='Send test letter') # true if passed
+    args = parser.parse_args()
 
-p.communicate(input=output.encode('utf-8'))
+    C = json.load(open('settings.json', 'r'))
+
+    if args.dry_run:
+        send_gmail(C['gmail'], 'subject', 'message')
+        exit()
+
+    logging.debug(sys.version)
+    logging.debug(str(sys.argv))
+
+    subject, message = parse_input(sys.argv[1:])
+    logging.info(subject)
+    logging.info(message)
+
+    send_gmail(C['gmail'], subject, message)
